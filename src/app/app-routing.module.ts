6a49584fb41910f6f922f4ca835a3fbcc9853057
Kgaogelo Thorn <kgaogelo@swipeix.com>
2023-03-24 08:55:09 +0200

    renamed folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
-import { HomeComponent } from './pages/home/home.component';
-import { AuthComponent } from './pages/auth/auth.component';
-import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './screens/home/home.component';
+import { AuthComponent } from './screens/auth/auth.component';
+import { LoginComponent } from './screens/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
-import { AuthComponent } from './pages/auth/auth.component';
-import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './screens/login/login.component';
+import { AuthComponent } from './screens/auth/auth.component';
+import { HomeComponent } from './screens/home/home.component';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, AuthComponent, HomeComponent],
