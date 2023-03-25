524922541675c478d700e7d5fe419e3b853b4866
Kgaogelo Thorn <kgaogelo@swipeix.com>
2023-03-25 19:31:14 +0200

    added user component

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -7,6 +7,7 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  isActiveGame: boolean = false;
   newGame: boolean = false;
   user$ = this.authService.user$;
 
